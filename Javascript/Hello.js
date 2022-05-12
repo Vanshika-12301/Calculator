8e85fa6dee682462f28de750ab76ea127a6214f0
Vanshika-12301 <102361254+Vanshika-12301@users.noreply.github.com>
2022-05-12 13:31:50 +0530

    Add files via upload

---
+++ Javascript/Hello.js
@@ -0,0 +1,13 @@
+
+let p2 = document.getElementsByClassName("p1");
+let p3 = document.getElementsByTagName("p");
+
+function change(){
+    let p1 = document.getElementById("para1");
+    p1.innerHTML="I am Paragraph 1" 
+} 
+
+function c(){
+    let p2 = document.getElementsByClassName("p1");
+    p2[0].innerHTML="Hello I am  Paragraph" 
+}
